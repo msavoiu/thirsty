b38a42a09f6737068ff265e8071e1ca3e4c06e71
msavoiu <savoiumadeline@gmail.com>
2025-08-17 19:38:51 -0700

    implement on click handler for map pan

--- thirsty/src/app/components/map.tsx
+++ thirsty/src/app/components/map.tsx
@@ -1,37 +1,64 @@
 "use client";
 
-import React from "react";
-import { APIProvider, Map, MapCameraChangedEvent, AdvancedMarker, Pin } from "@vis.gl/react-google-maps";
+import React, { useState, useCallback } from "react";
+import {
+    APIProvider,
+    Map,
+    MapCameraChangedEvent,
+    AdvancedMarker,
+    Pin,
+    useMap
+} from "@vis.gl/react-google-maps";
 
+// Components
+import MarkerPopup from "./marker_popup";
+
+// Type declarations for TS
 type MarkerMapProps = {
     apiKey: string;
     mapId: string;
 };
-
 type Poi ={ key: string, location: google.maps.LatLngLiteral }
 
+// Test marker for development purposes
 const locations: Poi[] = [
     {key: 'CSUF HRE', location: { lat: 33.88413084573613, lng: -117.88127569981039  }},
 ];
 
 const PoiMarkers = (props: {pois: Poi[]}) => {
-  return (
-    <>
-      {props.pois.map( (poi: Poi) => (
-        <AdvancedMarker
-          key={poi.key}
-          position={poi.location}>
-        <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
-        </AdvancedMarker>
-      ))}
-    </>
-  );
+    const map = useMap();
+
+    const handleClick = useCallback((ev: google.maps.MapMouseEvent) => {
+        if(!map) return;
+        if(!ev.latLng) return;
+
+        console.log("Marker clicked:", ev.latLng.toString());
+
+        // Pan so the selected marker is centered on the map
+        map.panTo(ev.latLng);
+        // Render popup component
+    
+    }, [map]);
+
+    return (
+        <>
+            {props.pois.map( (poi: Poi) => (
+                <AdvancedMarker
+                    key={poi.key}
+                    position={poi.location}
+                    onClick={handleClick}
+                >
+                    <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+                </AdvancedMarker>
+            ))}
+        </>
+    );
 };
 
 function MarkerMap({ apiKey, mapId }: MarkerMapProps) {
     return (
         <APIProvider apiKey={apiKey} onLoad={() => console.log("Maps API has loaded.")}>
-            <div style={{ width: "100%", height: "500px" }}>
+            <div style={{ width: "100%", height: "500px"}}>
                 <Map
                     mapId={mapId}
                     defaultZoom={13}
