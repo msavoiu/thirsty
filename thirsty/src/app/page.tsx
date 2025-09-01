189c2e373e831d284c2aea0a4b83597af7952c18
msavoiu <savoiumadeline@gmail.com>
2025-09-01 14:29:30 -0700

    add map component to homepage

--- thirsty/src/app/page.tsx
+++ thirsty/src/app/page.tsx
@@ -1,10 +1,20 @@
+import React from "react";
 import Image from "next/image";
 import Counter from "./components/counter";
+import MarkerMap from "./components/map";
+
 
 export default function Home() {
+
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string;
+  const mapId = process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID as string;
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        
+        <MarkerMap apiKey={apiKey} mapId={mapId}/>
+
         <Counter/>
 
         <a href="/log">
