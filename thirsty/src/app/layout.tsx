2216f31a77514935bfa71ca87ddc065d522d924c
Madeline Savoiu <savoiu@csu.fullerton.edu>
2025-11-11 11:42:37 -0800

    fixed filename case discrepancy

--- thirsty/src/app/layout.tsx
+++ thirsty/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 // Components
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/navbar";
 import { Footer } from "./components/Footer";
 import { UserProvider } from "../context/UserContext";
 
